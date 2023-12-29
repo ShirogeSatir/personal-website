b4517028dadaf261cb1d9a70603df46fc916d4f4
RaolMarketplace <raolmarketplace@gmail.com>
2023-12-29 19:58:59 +0700

    update

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -64,7 +64,7 @@ export default {
   },
   methods: {
     async getArticles() {
-      axios.get('https://64a38c9cc3b509573b564183.mockapi.io/api/blog/all')
+      axios.get('')
         .then(response => {
           this.articles = response.data;
         })
